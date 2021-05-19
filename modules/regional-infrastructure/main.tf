1625cbcafc4d15313e31ce6dcaca3e697acc9663
Brian Teller <brian.teller@formstack.com>
2021-05-19 11:00:21 -0400

    Update reference to module

--- modules/regional-infrastructure/main.tf
+++ modules/regional-infrastructure/main.tf
@@ -1,6 +1,7 @@
 module "test" {
   # Private Module
-  source = "git::https://github.com/bateller/dependabot-test-module.git?ref=tags/0.0.1"
+  # source = "git::https://github.com/bateller/dependabot-test-module.git?ref=tags/0.0.1"
+  source = "git@github.com:bateller/dependabot-test-module.git?ref=tags/0.0.1"
 
   s3-billing-bucket = "temp"
 }
