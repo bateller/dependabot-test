ebb80a7d739d00a9c5c49c8fb17dd23d0894beb9
Brian Teller <brian.teller@formstack.com>
2021-05-19 11:04:53 -0400

    Commit url changes for module

--- modules/regional-infrastructure/main.tf
+++ modules/regional-infrastructure/main.tf
@@ -2,6 +2,7 @@ module "test" {
   # Private Module
   # source = "git::https://github.com/bateller/dependabot-test-module.git?ref=tags/0.0.1"
   source = "git@github.com:bateller/dependabot-test-module.git?ref=tags/0.0.1"
+  # source = "git@github.com:bateller/dependabot-test-module.git"
 
   s3-billing-bucket = "temp"
 }
